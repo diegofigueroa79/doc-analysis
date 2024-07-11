68dde5af38d7338c88888244fc65ba2f270e8d91
Diego Figueroa <diegofigueroa79@gmail.com>
2024-07-11 15:30:24 -0700

    update tools

--- src/tools.py
+++ src/tools.py
@@ -1,18 +1,26 @@
 import textractcaller as tc
 from textractor.parsers import response_parser
 from langchain_aws import ChatBedrock
+from langchain_core.prompts import PromptTemplate
 import boto3
+import os
+import re
 
+
+QUERY_1 = "Choose from the three types to answer the question: balance sheet, income, cash flow. What type is the document?"
+QUERY_2 = "What is the company name?"
+QUERY_3 = "What is the month or date or year ended?"
 
 def extract_document():
     textract = boto3.client('textract', region_name='<region>')
-    q = tc.Query(text="Choose from the three types to answer the question: balance sheet, income, cash flow. What type is the document?", pages=["1", "2", "3"])
-    q2 = tc.Query(text="What is the company name?", pages=["1", "2", "3"])
-    adapter = tc.Adapter(adapter_id="<textract adapter id>", version="1", pages=["1", "2", "3"])
+    q = tc.Query(text=QUERY_1, pages=["*"])
+    q2 = tc.Query(text=QUERY_2, pages=["*"])
+    q3 = tc.Query(text=QUERY_3, pages=["*"])
+    adapter = tc.Adapter(adapter_id=os.getenv("TEXTRACT_ADAPTER_ID"), version="1", pages=["*"])
     
     result = tc.call_textract(
-        input_document="<bucket>combined-statements.pdf",
-        queries_config=tc.QueriesConfig(queries=[q,q2]),
+        input_document=f"{os.getenv("S3_BUCKET_URL")}combined-statements.pdf",
+        queries_config=tc.QueriesConfig(queries=[q,q2,q3]),
         adapters_config=tc.AdaptersConfig(adapters=[adapter]),
         features=[tc.Textract_Features.QUERIES, tc.Textract_Features.TABLES, tc.Textract_Features.LAYOUT],
         force_async_api=True,
@@ -24,13 +32,15 @@ def extract_document():
 
 def build_tables_dict(document):
     doc_tables = {}
+    financial_quarter = get_financial_quarter([q.result for q in document.queries if q.query == QUERY_3][0])
     
     for page in document.pages:
         # what is the doc type and company name?
-        company_name = page.queries[1].result
+        company_name = [q.result for q in document.queries if q.query == QUERY_2][0]
         if company_name not in doc_tables.keys():
             doc_tables[company_name] = {}
-        doc_type = page.queries[0].result
+            doc_tables['financial_quarter'] = financial_quarter
+        doc_type = [q.result for q in document.queries if q.query == QUERY_1][0]
         if doc_type not in doc_tables.keys():
             doc_tables[company_name][doc_type] = []
         # extract tables
@@ -39,11 +49,45 @@ def build_tables_dict(document):
 
     return doc_tables
 
-def generate_sql(llm, pd_table, db_schema):
+def get_financial_quarter(llm, date):
     # get prompt
+    template_text=open('prompt-financial-quarter.txt',"r").read()
+    template = PromptTemplate.from_template(template_text)
     # format prompt with table and schema
+    template.invoke(input={'date': date})
     # call llm
-    sql_tuples_list = []
+    response = llm.invoke(template)
+    content = extract_output_text(response.content)
+    return content
+
+def extract_output_text(input_text):
+    # Use regular expression to find text between <output> and </output> tags
+    pattern = r"<output>(.*?)<\/output>"
+    match = re.search(pattern, input_text, re.DOTALL)
+    
+    if match:
+        return match.group(1).strip()
+    else:
+        return None
+
+def parse_tuples(input_string):
+    input_string.replace('"', '')
+    # Remove leading/trailing whitespace and split by newline
+    lines = input_string.strip().split('\n')
+    # Parse each line as a tuple
+    result = [tuple(line.strip('()').split(', ')) for line in lines]
+    return result
+
+def generate_sql(llm, pd_table, db_schema, company_name, financial_quarter):
+    # get prompt
+    template_text=open('prompt-sql.txt',"r").read()
+    template = PromptTemplate.from_template(template_text)
+    # format prompt with table and schema
+    template.invoke(input={'content': pd_table, 'company_name': company_name, 'financial_quarter': financial_quarter})
+    # call llm
+    response = llm.invoke(template)
+    content = extract_output_text(response.content)
+    sql_tuples_list = parse_tuples(content)
     return sql_tuples_list
 
 def connect_to_bedrock():
