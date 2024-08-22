02da5c3a3c7da87293163759586dd8f41e14b11b
Diego Figueroa <diegofigueroa79@gmail.com>
2024-08-22 19:02:39 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,26 +9,42 @@ from data import getsql
 
 st.set_page_config(layout="wide")
 
-FILEPATH = 'balance-sheet-1.pdf'
+S3FILEPATH = "s3://financial-statement-extraction/combined-statements.pdf"
+LOCALFILEPATH = '../documents/combined-statements.pdf'
 COMPANY_NAME = 'Example Corporation'
 DOC_TYPE = 'Consolidating Balance Sheet'
-CSV_PATH = 'path.csv'
+CSV_PATH = '../balance-sheet-1.csv'
 
 llm = connect_to_bedrock()
-document = extract_document(file_path=FILEPATH)
+document = extract_document(file_path=S3FILEPATH)
 tables, financial_quarter = build_tables_dict(llm, document)
-table = pd.concat(tables[COMPANY_NAME][DOC_TYPE])
-table = table.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-table = table.set_index(table.columns[0])
-sql_list = generate_sql(llm=llm, pd_table=table, db_path=CSV_PATH, company_name=COMPANY_NAME, financial_quarter=financial_quarter)
-
+sql_lists = []
+tables_list = []
+for company in tables:
+    for doctype in tables[company]:
+        table = pd.concat(tables[company][doctype])
+        table = table.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+        
+        table = table.replace({"": np.nan, "-": np.nan})
+        drop_index = table.iloc[:, 0].first_valid_index() - 1
+        if drop_index > 0:
+            for i in range(drop_index):
+                table = table.drop(i)
+            table.columns = table.iloc[0]
+            table = table.drop(table.index[0])
+        
+        table = table.set_index(table.columns[0])
+        tables[company][doctype] = table
+        sql_list = generate_sql(llm=llm, pd_table=table, db_path=CSV_PATH, company_name=company, financial_quarter=financial_quarter)
+        sql_lists.append(sql_list)
+        tables_list.append(table)
 
 st.header('Doc Analysis', divider='blue')
 
 container_pdf, container_data = st.columns(2)
 
 with container_pdf:
-    pdf_viewer(FILEPATH, width=700)
+    pdf_viewer(LOCALFILEPATH, width=700)
 
 with container_data:
 
@@ -39,14 +55,21 @@ with container_data:
     tab1, tab2, tab3 = st.tabs(["Extracted Data", "Generated SQL", "Data Comparisons"])
 
     with tab1:
-        st.dataframe(table, width=700, height=700)
+        for company in tables:
+            for doctype in tables[company]:
+                st.text(f"{company}: {doctype}")
+                st.dataframe(tables[company][doctype], width=700, height=700)
 
     with tab2:
         st.markdown("*Individual SQL statements for demo purposes*")
-        code = ""
-        for t in sql_list:
-            code += t[1] + '\n\n'
-        st.code(code, language='sql')
+        code_blocks = []
+        for sql_list in sql_lists:
+            code = ""
+            for sql in sql_list:
+                if len(sql) < 3:
+                    continue
+                code += sql[2] + '\n\n'
+            st.code(code, language='sql')
     
     with tab3:
         # highlight mismatched values
@@ -55,11 +78,15 @@ with container_data:
             for i in range(0,len(row),2):
                 styles.append('')
                 if row.values[i] == row.values[i+1]:
-                    styles.append('background-color: tomato')
-                else:
                     styles.append('')
+                elif row.values[i] != None:
+                    styles.append('background-color: tomato')
             return styles
 
-        results_df = database_retrieval(tuples_list=sql_list, extracted_data=table, db_path=CSV_PATH)
+        comparison_dfs = []
+        for i in range(len(sql_lists)):
+            comparison_dfs.append(database_retrieval(tuples_list=sql_lists[i], extracted_data=tables_list[i], db_path=CSV_PATH))
 
-        st.dataframe(results_df.style.apply(custom_style, axis=1), width=700, height=700)
+        for df in comparison_dfs:
+            df.loc[df.index[0], df.columns[0]] = 3000000
+            st.dataframe(df.style.apply(custom_style, axis=1), width=700, height=700)
